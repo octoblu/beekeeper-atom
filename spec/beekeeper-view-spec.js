eed94e84e84c17d3c72201d881e467711a5fa845
Jade Meskill <jade.meskill@gmail.com>
2017-09-18 13:46:54 -0700

    the base atom package structure

---
+++ spec/beekeeper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BeekeeperView from '../lib/beekeeper-view';
+
+describe('BeekeeperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
